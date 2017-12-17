9170512ede190a0e7683285ae8c6f8fab0237dd4
Nicholas Ollis <nic@ollis.me>
2017-12-16 22:12:59 -0500

    Building out Managed Objects

---
+++ CoreData/Bow Ties/Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Nic Ollis on 12/16/17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
